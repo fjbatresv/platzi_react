3f95c714c9a94612fd38882562e91aa7aaeda9c2
Javier Batres <fjbatresv@gmail.com>
2020-07-21 22:46:39 -0600

    Module 5 of the Practical React Course finished

--- platzivideo/src/containers/App.jsx
+++ platzivideo/src/containers/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import RcIf, { RcElse } from 'rc-if';
 // Components
 import Header from '../components/Header';
 import Search from '../components/Search';
@@ -6,36 +7,38 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer/Footer';
+// Hook
+import useInitialState from '../hooks/useInitialState';
 // Style
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initalState';
+
 const App = () => {
+    const initialState = useInitialState(API);
     return (
         <div className="app">
             <Header />
             <Search />
-            <Categories title="A" >
+            <RcIf if={initialState.mylist.length > 0}>
+                <Categories title="Mi Lista" >
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            </RcIf>
+            <Categories title="Tendencias" >
                 <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
+                    {initialState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
                 </Carousel>
             </Categories>
-            <Categories title="B" >
+            <Categories title="Originales" >
                 <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
-            <Categories title="C" >
-                <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
+                    {initialState.originals.map(item =>
+                        <CarouselItem key={item.id} {...item} />
+                    )}
                 </Carousel>
             </Categories>
             <Footer />
